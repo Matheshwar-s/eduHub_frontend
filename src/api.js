165c89758c4810ac31aa20f8cbde1b96ba865f1c
Matheshwar S <matheshbusiness@gmail.com>
2025-09-09 16:31:49 +0530

    update7

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://eduhub-backend-kcnj.onrender.com",
+  withCredentials: false,
   //baseURL: "http://localhost:8080",
 });
 
